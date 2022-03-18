47cef6f1e027fb9ae8bca834eed0d8baed3f073e
Éverton M. Vieira <everton.muvi@gmail.com>
2022-03-18 11:42:34 -0300

    prototype development

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,44 @@
-import { QinColumn, QinLine, QinButton, QinLabel, QinExplorer } from "qinpel-cps"
+import {
+  QinColumn,
+  QinLine,
+  QinButton,
+  QinLabel,
+  QinExplorer,
+} from "qinpel-cps";
 
 class Abacuz extends QinColumn {
+  private qinTopBar = new QinLine();
+  private qinLoad = new QinButton({
+    label: new QinLabel("Load"),
+  });
+  private qinUp = new QinButton({
+    label: new QinLabel("Up"),
+  });
+  private qinExplorer = new QinExplorer();
 
-    private qinTopBar = new QinLine();
-    private qinTest = new QinButton({
-        label: new QinLabel("Test")
+  public constructor() {
+    super();
+    this.qinTopBar.install(this);
+    this.qinLoad.install(this.qinTopBar);
+    this.qinLoad.addAction((ev) => {
+      if (ev.isPrimary) {
+        this.qinExplorer.load("", (loaded) => {
+          this.qinpel().frame.statusInfo(loaded);
+        });
+      }
     });
-    private qinExplorer = new QinExplorer();
-
-    public constructor() {
-        super();
-        this.qinTopBar.install(this);
-        this.qinTest.install(this.qinTopBar);
-        this.qinTopBar.putAsFlexMin();
-        this.qinExplorer.install(this);
-        this.qinExplorer.putAsFlexMax();
-    }
-
+    this.qinUp.install(this.qinTopBar);
+    this.qinUp.addAction((ev) => {
+      if (ev.isPrimary) {
+        this.qinExplorer.goFolderUp((loaded) => {
+          this.qinpel().frame.statusInfo(loaded);
+        });
+      }
+    });
+    this.qinTopBar.putAsFlexMin();
+    this.qinExplorer.install(this);
+    this.qinExplorer.putAsFlexMax();
+  }
 }
 
 new Abacuz().putAsBody();
